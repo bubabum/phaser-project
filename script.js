bc164cb12e6ba029762aad7a3d6786802f28959c
bubabum <bubabum.drt@gmail.com>
2023-02-21 17:00:25 +0200

    platforms added

--- script.js
+++ script.js
@@ -27,29 +27,32 @@ let config = {
 
 let player;
 let cursors;
-
 let map;
 let tileset;
 let layer;
+let platforms;
+
+let follower;
+let path;
 
 let camera;
+
+let bombs;
+let bomb;
+
+let lastFired = 0;
 
 let game = new Phaser.Game(config);
 
 function preload() {
 	this.load.image('tiles', './assets/tile_map.png');
+	this.load.image('platform', './assets/platform.png');
 	this.load.tilemapTiledJSON('map', './assets/lv.json');
 	this.load.spritesheet('player', './assets/player.png', { frameWidth: 58, frameHeight: 58 });
+	this.load.spritesheet('bomb', 'assets/bomb.png', { frameWidth: 96, frameHeight: 108 });
 }
 
 function create() {
-	map = this.make.tilemap({ key: 'map', tileWidth: 64, tileHeight: 64 });
-	tileset = map.addTilesetImage('tile_map', 'tiles');
-	layer = map.createLayer('ground', tileset);
-	map.setCollision([0, 1, 2, 6, 7, 8, 12, 13, 14, 18, 19, 24, 25]);
-
-	this.physics.world.setBounds(0, 0, 35 * TILE, 25 * TILE);
-
 	this.anims.create({
 		key: 'idle',
 		frames: this.anims.generateFrameNumbers('player', {
@@ -96,11 +99,60 @@ function create() {
 		repeat: 0,
 	});
 
-	player = this.physics.add.sprite(400, 300, 'player');
-	player.setCollideWorldBounds(true);
+	this.anims.create({
+		key: 'on',
+		frames: this.anims.generateFrameNumbers('bomb', {
+			start: 0,
+			end: 9,
+		}),
+		frameRate: 20,
+		repeat: -1,
+	});
+	map = this.make.tilemap({ key: 'map', tileWidth: 64, tileHeight: 64 });
+	tileset = map.addTilesetImage('tile_map', 'tiles');
+	layer = map.createLayer('ground', tileset);
+	map.setCollision([1, 2, 3, 7, 8, 9, 13, 14, 15, 19, 20, 25, 26]);
+	platforms = map.createLayer('platforms', tileset);
+	platforms.forEachTile(tile => {
+		let colissionIndexArray = [23, 24, 29, 30];
+		if (colissionIndexArray.includes(tile.index)) {
+			tile.setCollision(false, false, true, false);
+		}
+	});
+
+
+	// let mov = map.createFromObjects('platforms1', { gid: 30, key: 'platform' });
+	// const plats = this.physics.add.group(mov);
+	// plats.getChildren().forEach(plat => {
+	// 	plat.body.setImmovable(true);
+	// 	plat.body.allowGravity = false;
+	// 	plat.body.checkCollision.left = false;
+	// 	plat.body.checkCollision.right = false;
+	// 	plat.body.checkCollision.down = false;
+	// })
+
+	//this.physics.world.setBounds(0, 0, 35 * TILE, 25 * TILE);
+
+	player = this.physics.add.sprite(1600, 300, 'player');
+	player.setSize(25, 50, false);
+	player.body.offset.x = 20;
+	player.body.offset.y = 8;
+	//player.setCollideWorldBounds(true);
 	player.anims.play('idle');
 
+
+	bombs = this.physics.add.group({
+		angularVelocity: 0,
+		angularAcceleration: 0,
+		velocityX: 200,
+		maxSize: 3,
+		defaultKey: 'bomb',
+	});
+
 	this.physics.add.collider(player, layer);
+	this.physics.add.collider(player, platforms);
+	this.physics.add.collider(player, bombs);
+	this.physics.add.collider(layer, bombs);
 
 	player.on(Phaser.Animations.Events.ANIMATION_COMPLETE_KEY + 'land', function (anims) {
 		player.chain(['land', 'idle']);
@@ -108,12 +160,25 @@ function create() {
 
 	cursors = this.input.keyboard.createCursorKeys();
 
+	this.input.keyboard.on('keydown-SPACE', function (event) {
+
+		event.stopPropagation();
+		bomb = bombs.create(player.x, player.y, 'bomb');
+		bomb.setCircle(15).setOrigin(0.51, 0.67).setBounce(0.5);
+		bomb.body.offset.x = 34;
+		bomb.body.offset.y = 58;
+		bomb.body.setImmovable(false);
+		bomb.anims.play('on');
+		setTimeout(() => bomb.destroy(), 500)
+
+	});
+
 	camera = this.cameras.main;
-	camera.setBounds(0, 0, 35 * TILE, 25 * TILE);
+	camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 	camera.startFollow(player, true, 1, 1, 0, 0);
 }
 
-function update() {
+function update(time, delta) {
 	if (cursors.left.isDown) {
 		player.setVelocityX(-160);
 		player.flipX = true;
@@ -130,7 +195,7 @@ function update() {
 		player.allowedToJump = true;
 	}
 	if (cursors.up.isDown && player.body.blocked.down && player.allowedToJump) {
-		player.setVelocityY(-350);
+		player.setVelocityY(-320);
 		player.anims.play('jump', true);
 		player.allowedToJump = false;
 		player.hitGround = false;
@@ -142,4 +207,22 @@ function update() {
 		player.hitGround = true;
 		player.anims.play('land', true);
 	}
+	if (cursors.space.isDown) {
+		// bomb = bombs.create(player.x, player.y, 'bomb');
+		// bomb.setCircle(15).setOrigin(0.51, 0.67).setBounce(0.5);
+		// bomb.body.offset.x = 34;
+		// bomb.body.offset.y = 58;
+		// bomb.body.setImmovable(false);
+		// bomb.anims.play('on');
+	}
+
+	// if (bomb.body.angularVelocity < 0) {
+	// 	bomb.body.angularAcceleration = 0;
+	// }
+	// if (bomb.body.velocity.x > 0) {
+	// 	bomb.body.velocity.x -= 1;
+	// }
+	// if (bomb.body.velocity.x < 0) {
+	// 	bomb.body.velocity.x += 1;
+	// }
 }
