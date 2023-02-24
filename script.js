62a2ca9d4149560a727f7069dc36bde5dadf0e09
bubabum <bubabum.drt@gmail.com>
2023-02-24 16:57:48 +0200

    enemy capitan AI

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let config = {
 		default: 'arcade',
 		arcade: {
 			gravity: { y: 400 },
-			debug: false,
+			debug: true,
 		},
 	},
 	scene: {
@@ -42,6 +42,8 @@ let bombVelocity = 0;
 let bombMaxVelocity = 300;
 let bombBar;
 
+let capitansGroup;
+
 let lastFired = 0;
 
 let game = new Phaser.Game(config);
@@ -52,7 +54,7 @@ function preload() {
 	this.load.spritesheet('player', './assets/player.png', { frameWidth: 58, frameHeight: 58 });
 	this.load.spritesheet('bomb', 'assets/bomb.png', { frameWidth: 96, frameHeight: 108 });
 	this.load.spritesheet('bar', 'assets/bar.png', { frameWidth: 39, frameHeight: 9 });
-	this.load.spritesheet('capitan', 'assets/enemy_capitan.png', { frameWidth: 80, frameHeight: 72 });
+	this.load.spritesheet('capitan', 'assets/capitan.png', { frameWidth: 80, frameHeight: 72 });
 }
 
 function create() {
@@ -165,7 +167,7 @@ function create() {
 
 	map = this.make.tilemap({ key: 'map', tileWidth: 64, tileHeight: 64 });
 	tileset = map.addTilesetImage('tile_map', 'tiles');
-	layer = map.createLayer('ground', tileset);
+	groundLayer = map.createLayer('ground', tileset);
 	map.setCollision([1, 2, 3, 7, 8, 9, 13, 14, 15, 19, 20, 25, 26]);
 	platforms = map.createLayer('platforms', tileset);
 	platforms.forEachTile(tile => {
@@ -193,22 +195,26 @@ function create() {
 		defaultKey: 'bomb',
 	});
 
-	let capitansGroup = this.physics.add.group();
+	capitansGroup = this.physics.add.group();
 	let capitansArray = map.createFromObjects('enemies', [{ gid: 31, key: 'capitan', frame: 0 }]);
 
 	for (var i = 0; i < capitansArray.length; i++) {
 		capitansGroup.add(capitansArray[i]);
 		capitansArray[i].body.setSize(30, 67, false).setOffset(20, 5);
 		capitansArray[i].anims.create({
-			key: 'idle',
-			frames: this.anims.generateFrameNumbers('capitan', {
-				start: 0,
-				end: 31,
-			}),
+			key: 'run',
+			frames: this.anims.generateFrameNumbers('capitan', { start: 32, end: 45 }),
 			frameRate: 20,
 			repeat: -1,
 		});
-		capitansArray[i].anims.play('idle');
+		capitansArray[i].anims.create({
+			key: 'atack',
+			frames: this.anims.generateFrameNumbers('capitan', { start: 56, end: 62 }),
+			frameRate: 20,
+			repeat: -1,
+		});
+		capitansArray[i].anims.play('run');
+		capitansArray[i].body.setVelocityX(120);
 	}
 
 
@@ -216,11 +222,11 @@ function create() {
 		player.chain(['land', 'idle']);
 	}, this);
 
-	this.physics.add.collider(player, layer);
+	this.physics.add.collider(player, groundLayer);
 	this.physics.add.collider(player, platforms);
-	this.physics.add.collider(capitansGroup, layer);
+	this.physics.add.collider(capitansGroup, groundLayer);
 	this.physics.add.collider(capitansGroup, platforms);
-	this.physics.add.collider(bombs, layer);
+	this.physics.add.collider(bombs, groundLayer);
 	this.physics.add.collider(bombs, platforms);
 
 	cursors = this.input.keyboard.createCursorKeys();
@@ -269,6 +275,7 @@ function create() {
 }
 
 function update() {
+	let game = this;
 	if (cursors.left.isDown && player.anims.currentAnim.key !== 'hit') {
 		player.setVelocityX(-160);
 		//player.flipX = true;
@@ -313,4 +320,40 @@ function update() {
 	})
 	bombBar.setPosition(player.getCenter().x + 3, player.getCenter().y - 30)
 
+	capitansGroup.getChildren().forEach(capitan => {
+		walkingBehavior(groundLayer, capitan);
+		if (Phaser.Math.Distance.BetweenPoints(player, capitan) < 40 && capitan.anims.currentAnim.key !== 'atack') {
+			capitan.body.stop();
+			capitan.anims.play('atack');
+		}
+		if (Phaser.Math.Distance.BetweenPoints(player, capitan) > 40 && capitan.anims.currentAnim.key !== 'run') {
+			capitan.body.setVelocityX(120);
+			capitan.anims.play('run');
+		}
+	})
+}
+
+function walkingBehavior(layer, npc) {
+	const isLeftOrientated = (npc) => {
+		return npc.body.velocity.x < 0
+	}
+	const isNextGroundTileCollidable = (layer, npc, isLeftOrientated) => {
+		const { x, y, width, height } = npc.body;
+		const posX = x + (isLeftOrientated ? -0.5 : width + 0.5);
+		const tile = layer.getTileAtWorldXY(posX, y + height + 0.5);
+		return tile?.collideUp
+	}
+	const isNextTileCollidable = (layer, npc, isLeftOrientated) => {
+		const { x, y, width, height } = npc.body;
+		const posX = x + (isLeftOrientated ? -0.5 : width + 0.5);
+		const tile = layer.getTileAtWorldXY(posX, y + height - 0.5);
+		if (isLeftOrientated) return tile?.collideRight;
+		return tile?.collideLeft;
+	}
+	const nextGroundTile = isNextGroundTileCollidable(layer, npc, isLeftOrientated(npc))
+	const nextTile = isNextTileCollidable(layer, npc, isLeftOrientated(npc))
+	if (!nextGroundTile || nextTile) {
+		npc.body.setVelocityX(-npc.body.velocity.x);
+		npc.toggleFlipX();
+	}
 }
